c338d9eb5591db69747898a6840025852657f903
Daniel Vu <dvv297@gmail.com>
2018-03-11 13:14:44 -0500

    Add preliminary InteractionIcon, finishing touches

--- src/components/InteractionEntry/InteractionEntry.jsx
+++ src/components/InteractionEntry/InteractionEntry.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './InteractionEntry.css';
 import InteractionShape from '../../proptypes/InteractionShape';
 import TimeEntry from '../TimeEntry/TimeEntry';
+import InteractionIcon from '../InteractionIcon/InteractionIcon';
 import InteractionEntryCard from '../InteractionEntryCard/InteractionEntryCard';
 
 const InteractionEntry = ({ interaction }) => {
@@ -10,6 +11,9 @@ const InteractionEntry = ({ interaction }) => {
     <div className="interaction-entry">
       <div className="interaction-entry-timeentry-container">
         <TimeEntry time={interaction.time} />
+      </div>
+      <div className="interaction-entry-icon-container">
+        <InteractionIcon />
       </div>
       <div className="interaction-entry-card-container">
         <InteractionEntryCard interaction={interaction} />
---
+++ src/components/InteractionIcon/InteractionIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './InteractionIcon.css';
+
+const InteractionIcon = () => {
+  return <div className="interaction-icon" />
+};
+
+export default InteractionIcon;
